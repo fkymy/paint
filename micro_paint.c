e843500055b049cdc96a62fe6e4a457a58855215
fkymy <yuskefuku@gmail.com>
2020-11-24 17:23:23 +0900

    happy painting

--- micro_paint.c
+++ micro_paint.c
@@ -1,38 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-#define ERR_OP "Error: Operation file corrupted"
-
-#define die(msg, fp) ({\
-		write(1, msg, ft_strlen(msg));\
-		write(1, "\n", 1);\
+#define die(fp) ({\
+		write(1, "Error: Operation file corrupted\n", 32);\
 		if (fp) fclose(fp);\
 		return 1;})\
 
-size_t ft_strlen(char *s)
-{
-	char *p = s;
-	while (*p)
-		p++;
-	return (p - s);
-}
-
-void *ft_memset(void *s, int c, size_t n)
-{
-	char *ss = s;
-	for (size_t i = 0; i < n; i++)
-		ss[i] = c;
-	return (s);
-}
-
-int is_range(int len)
-{
-	return (len > 0 && len <= 300);
-}
-
 struct rect {
-	float tlx, tly, w, h;
+	float xtl, ytl, w, h;
 	char type, c;
 };
 
@@ -44,16 +21,17 @@ int is_rect(struct rect *rect)
 
 int should_fill(int x, int y, struct rect *rect)
 {
-	int brx = rect->tlx + rect->w;
-	int bry = rect->tly + rect->h;
+	float xa = x, ya = y;
+	float xbr = rect->xtl + rect->w;
+	float ybr = rect->ytl + rect->h;
 
-	if (x < rect->tlx || y < rect->tly)
+	if (xa < rect->xtl || ya < rect->ytl)
 		return (0);
-	if (x > brx || y > bry)
+	if (xa > xbr || ya > ybr)
 		return (0);
-	if (x - rect->tlx < 1 || y - rect->tly < 1)
+	if (xa - rect->xtl < 1 || ya - rect->ytl < 1)
 		return (1);
-	if (rect->tlx + rect->w - x < 1 || rect->tly + rect->h - y < 1)
+	if (xbr - xa < 1 || xbr - ya < 1)
 		return (1);
 	return (rect->type == 'R' ? 1 : 0);
 }
@@ -64,10 +42,12 @@ int main(int argc, char *argv[])
 	FILE *fp;
 
 	if (argc != 2)
-		die("Error: argument", 0);
-
+	{
+		write(1, "Error: argument\n", 16);
+		return (1);
+	}
 	if (!(fp = fopen(argv[1], "r")))
-		die(ERR_OP, 0);
+		die(NULL);
 
 	// 1. Parse zone
 	struct zone {
@@ -76,29 +56,30 @@ int main(int argc, char *argv[])
 	} zone;
 
 	if (!(fscanf(fp, "%d %d %c\n", &zone.w, &zone.h, &zone.c) == 3))
-		die(ERR_OP, fp);
-	if (!is_range(zone.w) || !is_range(zone.h))
-		die(ERR_OP, fp);
+		die(fp);
+	if (!(zone.w > 0 && zone.w <= 300 && zone.h > 0 && zone.h <= 300))
+		die(fp);
 
-	ft_memset(output, zone.c, zone.w * zone.h);
+	memset(output, zone.c, zone.w * zone.h);
 
 	// 3. Parse and apply Rect
 	struct rect rect;
 
 	int n;
 	while ((n = fscanf(fp, "%c %f %f %f %f %c\n",
-					&rect.type, &rect.tlx, &rect.tly,
+					&rect.type, &rect.xtl, &rect.ytl,
 					&rect.w, &rect.h, &rect.c)) == 6)
 	{
 		if (!is_rect(&rect))
-			die(ERR_OP, fp);
+			die(fp);
+
 		for (int y = 0; y < zone.h; y++)
 			for (int x = 0; x < zone.w; x++)
 				if (should_fill(x, y, &rect))
 					output[y * zone.w + x] = rect.c;
 	}
 	if (n != EOF)
-		die(ERR_OP, fp);
+		die(fp);
 
 	// 2. Output
 	for (int i = 0; i < zone.h; i++)
--- mini_paint.c
+++ mini_paint.c
@@ -1,38 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <math.h>
 
-#define ERR_OP "Error: Operation file corrupted"
-
-#define die(msg, fp) ({\
-		write(1, msg, ft_strlen(msg));\
-		write(1, "\n", 1);\
+#define die(fp) ({\
+		write(1, "Error: Operation file corrupted\n", 32);\
 		if (fp) fclose(fp);\
 		return 1;})\
-
-size_t ft_strlen(char *s)
-{
-	char *p = s;
-	while (*p)
-		p++;
-	return (p - s);
-}
-
-void *ft_memset(void *s, int c, size_t n)
-{
-	char *ss = s;
-
-	for (size_t i = 0; i < n; i++)
-		ss[i] = c;
-	return (s);
-}
-
-int is_range(int len)
-{
-	return (len > 0 && len <= 300);
-}
-
 
 struct circle {
 	float x, y, r;
@@ -46,9 +21,9 @@ int is_circle(struct circle *circle)
 
 int should_fill(int x, int y, struct circle *circle)
 {
-	float dist;
 	float xa = x, ya = y;
 	float xb = circle->x, yb = circle->y;
+	float dist;
 
 	dist = sqrtf((xa - xb) * (xa - xb) + (ya - yb) * (ya - yb));
 	if (dist > circle->r)
@@ -64,10 +39,12 @@ int main(int argc, char *argv[])
 	FILE *fp;
 
 	if (argc != 2)
-		die("Error: argument\n", NULL);
-
+	{
+		write(1, "Error: argument\n", 16);
+		return (1);
+	}
 	if (!(fp = fopen(argv[1], "r")))
-		die(ERR_OP, fp);
+		die(fp);
 
 	// 1. Parse zone
 	struct zone {
@@ -76,11 +53,11 @@ int main(int argc, char *argv[])
 	} zone;
 
 	if (fscanf(fp, "%d %d %c\n", &zone.w, &zone.h, &zone.c) != 3)
-		die(ERR_OP, fp);
-	if (!is_range(zone.w) || !is_range(zone.h))
-		die(ERR_OP, fp);
+		die(fp);
+	if (!(zone.w > 0 && zone.w <= 300 && zone.h > 0 && zone.h <= 300))
+		die(fp);
 
-	ft_memset(output, zone.c, zone.w * zone.h);
+	memset(output, zone.c, zone.w * zone.h);
 
 	// 3. Parse and apply circles
 	struct circle circle;
@@ -91,14 +68,15 @@ int main(int argc, char *argv[])
 					&circle.r, &circle.c)) == 5)
 	{
 		if (!is_circle(&circle))
-			die(ERR_OP, fp);
+			die(fp);
+
 		for (int y = 0; y < zone.h; y++)
 			for (int x = 0; x < zone.w; x++)
 				if (should_fill(x, y, &circle))
 					output[y * zone.w + x] = circle.c;
 	}
 	if (n != EOF)
-		die(ERR_OP, fp);
+		die(fp);
 
 	// 2. Output
 	for (int i = 0; i < zone.h; i++)
